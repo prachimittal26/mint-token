efc0f9b393027eef387ee3d56200ba19a9ca99ee
Prachi <109284439+prachimittal26@users.noreply.github.com>
2024-06-27 01:16:38 +0530

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    address public owner;
+
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1000 * 10 ** decimals()); // Mint initial supply to contract deployer
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner, "Only owner can mint tokens");
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
